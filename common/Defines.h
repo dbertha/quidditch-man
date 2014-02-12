ec5c5683bd3027276921a1b0bc95384bc9a3e32f
Hélène Plisnier <helene.plisnier@ulb.ac.be>
2014-02-12 20:55:50 +0100

    [feature] Server.
    Change-Id: I8381544358e9acb277b2c0912f5892e087125e78

--- common/Defines.h
+++ common/Defines.h
@@ -1,5 +1,4 @@
 #define PORT 3495
 #define BACKLOG 20 //nbre mac de clients dans la file d'attente
-#define TRUE 1
-#define FALSE 0
 #define ERROR -1
+#define INPUTSIZE 100 //taille du message envoyé du client vers le serveur
