99f1225bd2122d9f47cbd3c7e5cd0733c210efac
David Bertha <dbertha@ulb.ac.be>
2014-02-13 13:47:34 +0100

    Revert "[feature] Server."
    This reverts commit ec5c5683bd3027276921a1b0bc95384bc9a3e32f.

--- common/Server.h
+++ common/Server.h
@@ -1,10 +1,6 @@
 #ifndef SERVER_H
 #define SERVER_H
-
-#include "Defines.h"
 #include <iostream>
-#include <set>
-#include <vector>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -17,36 +13,20 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-class User;
 class Server {
 public:
 	Server(int);
 	void run();
-
 
 private:
 	int port_;
 	int max_; //nombre de connections
 	int sockfd_; //socket d'écoute du serveur
 	struct sockaddr_in sockAddress_;
-	int clientSockfd_;
-	struct sockaddr_in clientSockAddress_;
 	unsigned int sin_size;
-	char msg[INPUTSIZE];
-	fd_set FDSet_;
-	std::vector<User*> usersList_;
 
 	int connect();
-	int mainLoop();
 	void disconnect();
-	void loadFDSet();
-	bool isNewConnection();
-	bool keyboard();
-	bool newUser();
-	int receive(User *, char *);
-	int sendToClient(User *, std::string);
-	void removeUser(int);
-	void removeAllUsers();
 };
 
 #endif
