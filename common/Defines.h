f84a3cb3ba424284b85026a8f4c9117ffaa3e2fa
David Bertha <dbertha@ulb.ac.be>
2014-02-13 13:47:34 +0100

    Revert "[feature] Server."
    This reverts commit ec5c5683bd3027276921a1b0bc95384bc9a3e32f.

--- common/Defines.h
+++ common/Defines.h
@@ -1,4 +1,5 @@
 #define PORT 3495
 #define BACKLOG 20 //nbre mac de clients dans la file d'attente
+#define TRUE 1
+#define FALSE 0
 #define ERROR -1
-#define INPUTSIZE 100 //taille du message envoyé du client vers le serveur
