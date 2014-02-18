924be1682833362aa4533a02594cd1aced5db55e
Manon Legrand <manolegr@ulb.ac.be>
2014-02-18 23:26:16 +0100

    [fix] Calender.cpp : rectification calcul du mois
    Change-Id: Ia25d6c4bf87e00fdb2598a6116b02dbb9cee88fd

--- common/Calendar.cpp
+++ common/Calendar.cpp
@@ -106,7 +106,7 @@ bool Calendar::checkBlock(char* line) {
 					if ( ((day==32)&&((month==1)||(month==3)||(month==5)||(month==7)||(month==8)||(month==10)||(month==12))) || \
 					( (day==31)&&((month==4)||(month==6)||(month==9)||(month==11)) ) || ( (day==29)&&(month=2) ) ) {
 						month+=1;
-						month=month%12;
+						if (month==13) month=1;
 						day=1;
 					} 
 				}
